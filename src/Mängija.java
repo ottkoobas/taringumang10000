eb9739ad51f9f7dcce7e6e07e02e4fb0f05fe8c4
hardopost <hardopost@gmail.com>
2020-03-03 15:55:30 +0200

    lisasin klassi

---
+++ src/Mängija.java
@@ -0,0 +1,49 @@
+public class Mängija {
+    private String nimi;
+    private int [] visketulemus = new int[5];
+    private int puktideSumma;
+
+//konstruktor, mis loob mängija isendi
+    public Mängija(String nimi) {
+        this.nimi = nimi;
+        this.visketulemus = visketulemus;
+        this.puktideSumma = puktideSumma;
+    }
+
+    // tavalised meetodid
+    // tagastab ühe viisiku viske tulemused massiivina
+    public int[] viienevise() {
+        int[] viienevise = new int[5];
+        for (int i = 0; i < viienevise.length; i++) {
+            int suvaline1_6 = (int) ((Math.random() * 10 * 0.6) + 1);
+            viienevise[i] = suvaline1_6;
+        }
+    return viienevise;
+    }
+
+
+    //get ja set meedotid
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public int[] getVisketulemus() {
+        return visketulemus;
+    }
+
+    public void setVisketulemus(int[] visketulemus) {
+        this.visketulemus = visketulemus;
+    }
+
+    public int getPuktideSumma() {
+        return puktideSumma;
+    }
+
+    public void setPuktideSumma(int puktideSumma) {
+        this.puktideSumma = puktideSumma;
+    }
+}
