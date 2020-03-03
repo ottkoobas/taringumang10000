282616d99bbfa41b8e61ac780a4818d6e004597b
Ott Koobas <ott@luxit.ee>
2020-03-03 15:02:19 +0200

    Create vise.java
    esmane katse

---
+++ vise.java
@@ -0,0 +1,15 @@
+public class vise {
+    int yks = 100;
+    int viis = 50;
+
+    /*3x yks = 1000;
+    3x kaks = 200;
+    3x kolm = 300;
+    3x neli = 400;
+    3x viis = 500;
+    3x kuus = 600;
+
+     */
+
+
+}
