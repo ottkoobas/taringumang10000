5460cd11410eac67a38761f3a114d0d1f578a1e3
Ott Koobas <ott@luxit.ee>
2020-03-03 15:51:01 +0200

    tegija
    zxc
    
    Co-Authored-By: hardopost <hardopost@users.noreply.github.com>

---
+++ src/ViskeSkoor.java
@@ -0,0 +1,25 @@
+public class ViskeSkoor {
+    int skoor;
+    int viske_skoor;
+
+    public int getSkoor() {
+        return skoor;
+    }
+
+    public void setSkoor(int skoor) {
+        this.skoor = skoor;
+    }
+
+    public int getViske_skoor() {
+        return viske_skoor;
+    }
+
+    public void setViske_skoor(int viske_skoor) {
+        this.viske_skoor = viske_skoor;
+    }
+
+    public ViskeSkoor(int skoor, int viske_skoor) {
+        this.skoor = skoor;
+        this.viske_skoor = viske_skoor;
+    }
+}
